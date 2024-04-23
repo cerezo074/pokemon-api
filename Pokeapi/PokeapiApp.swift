06b497ff253a1e61795a7d846d88d3b617c1baef
eli <eli@elis-MacBook-Pro.local>
2024-04-23 09:45:22 -0500

    Add initial project

---
+++ Pokeapi/PokeapiApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeapiApp.swift
+//  Pokeapi
+//
+//  Created by eli on 23/04/24.
+//
+
+import SwiftUI
+
+@main
+struct PokeapiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
