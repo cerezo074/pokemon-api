cad80c5894c17135024d2277db9c493d2ec75836
eli <eli@elis-MacBook-Pro.local>
2024-04-26 14:25:06 -0500

    Add search functionality

--- Pokeapi/Shared/Services/Repositories/Pokemon/MockObjects.swift
+++ Pokeapi/Shared/Services/Repositories/Pokemon/MockObjects.swift
@@ -8,6 +8,34 @@
 import Foundation
 import PokemonAPI
 
+struct MockObjects {
+    
+    static let retryPokemonID = "RETRY"
+    static var retryPokemonViewModel: PokemonCatalogItemViewModel {
+        PokemonCatalogItemViewModel.init(
+            name: Self.retryPokemonID,
+            id: Self.retryPokemonID,
+            types: [Self.retryPokemonID, Self.retryPokemonID],
+            pokemonImageURL: nil,
+            backgroundImageURL: nil,
+            itemStyle: .retry
+        )
+    }
+
+    static let loadMorePokemonID = "LOADING..."
+    static var loadMorePokemonViewModel: PokemonCatalogItemViewModel {
+        PokemonCatalogItemViewModel(
+            name: Self.loadMorePokemonID,
+            id: Self.loadMorePokemonID,
+            types: [Self.loadMorePokemonID, Self.loadMorePokemonID],
+            pokemonImageURL: nil,
+            backgroundImageURL: nil,
+            itemStyle: .load
+        )
+    }
+    
+}
+
 extension PKMPagedObject<PKMPokemon> {
     
     static func makePagination() -> PKMPagedObject<PKMPokemon> {
